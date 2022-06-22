f05c0401e6c94cde13794336b98aa31bd5c1608e
Yash Shah <yashs4062@gmail.com>
2022-06-22 17:15:36 +0530

    first commit

---
+++ app/src/main/java/com/example/attendanceapp/LandingActivity.java
@@ -0,0 +1,25 @@
+package com.example.attendanceapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class LandingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_landing);
+
+        new Timer().schedule(new TimerTask(){
+            public void run() {
+                startActivity(new Intent(LandingActivity.this, MainActivity.class));
+                finish();
+            }
+        }, 1000 );
+    }
+}
